14e70d16eb29f5f8ee49ee69dab18fa048fa83a5
Georgii Gerasev <george30032002@gmail.com>
2025-05-01 15:28:14 +0400

    feat: simple config check

--- packages/cli/src/ProjectConfig.ts
+++ packages/cli/src/ProjectConfig.ts
@@ -33,6 +33,8 @@ const ProjectConfigSchema = Schema.Struct({
   nodes: Schema.Array(NodeSchema),
 })
 
+type ProjectConfigType = Schema.Schema.Type<typeof ProjectConfigSchema>
+
 export class ProjectConfig extends Effect.Service<ProjectConfig>()(
   "ProjectConfig",
   {
@@ -41,10 +43,48 @@ export class ProjectConfig extends Effect.Service<ProjectConfig>()(
       const fs = yield* FileSystem.FileSystem;
       const configRaw = yield* fs.readFileString(path.join(path.resolve(), "config.json"));
       // TODO: add environment configuration lookups
-      return Schema.decodeUnknownSync(ProjectConfigSchema)(configRaw)
+      const config = Schema.decodeUnknown(ProjectConfigSchema)(configRaw)
+      return yield* checkConfig(config)
     })
   },
 ) {}
+
+function checkConfig(effectConfig: Effect.Effect<ProjectConfigType, Error, never>) : Effect.Effect<ProjectConfigType, Error, never> {
+  return Effect.gen(function* () {
+    const config = yield* effectConfig
+
+    if (!(config.network == "Preprod")) {
+      Error("The network is not Preprod")
+    }
+
+    if ("blockfrostProjectId" in config.providerId) {
+      if (!(config.providerId.blockfrostProjectId.startsWith(config.network.toLowerCase()))) {
+        Error("The blockfrostProjectId is from a wrong network")
+      }
+    }
+
+    // TODO: The same for Koios?
+
+    const nodes = config.nodes
+    const walletSKs = (nodes.map((node) => node.fundsWalletSK).concat(nodes.map((node) => node.nodeWalletSK)))
+    if (!(walletSKs.map((sk) => sk.type).every((type) => type == "PaymentSigningKeyShelley_ed25519"))) {
+      Error("One wallet secret key or more have non PaymentSigningKeyShelley_ed25519 type field")
+    }
+    if (!(walletSKs.map((sk) => sk.cborHex).every((hex) => hex.startsWith("5820")))) {
+      Error("One wallet secret key or more starts not with 5820")
+    }
+
+    const hydraSKs = nodes.map((node) => node.hydraSK)
+    if (!(hydraSKs.map((sk) => sk.type).every((type) => type == "HydraSigningKey_ed25519"))) {
+      Error("One hydra secret key or more have non HydraSigningKey_ed25519 type field")
+    }
+    if (!(hydraSKs.map((sk) => sk.cborHex).every((hex) => hex.startsWith("5820")))) {
+      Error("One hydra secret key or more starts not with 5820")
+    }
+
+    return config
+  })
+}
 
 // Simulate a project config for testing purposes
 export const testLayer = Layer.succeed(
