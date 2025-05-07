14e056e848d4eee53e65806a97480b89575e79dd
solidsnakedev <jona.ca.eng@gmail.com>
2025-05-07 15:14:19 -0600

    feat: add pubsub

--- packages/cli/src/Socket.ts
+++ packages/cli/src/Socket.ts
@@ -1,5 +1,5 @@
 import { Socket } from "@effect/platform";
-import { Effect, Queue } from "effect";
+import { Effect, PubSub } from "effect";
 
 export const createWebSocketConnection = (url: string) =>
   Effect.gen(function* () {
@@ -9,39 +9,36 @@ export const createWebSocketConnection = (url: string) =>
     const socket = yield* Socket.makeWebSocket(url);
 
     // Create an unbounded queue for messages
-    const messages = yield* Queue.unbounded<Uint8Array>();
+    const messages = yield* PubSub.unbounded<Uint8Array>();
     yield* Effect.log("Message queue created");
 
-    // Start a fiber that continuously processes incoming messages into the queue
-    yield* Effect.log("Starting WebSocket message handler fiber");
-    const fiber = yield* Effect.fork(
+    /*
+     * Start a fiber that continuously processes incoming messages into the pubsub queue
+     */
+    const publishMessageFiber = Effect.fork(
       socket
         .run((data) => {
           return Effect.gen(function* () {
             yield* Effect.log(
-              `WebSocket message received in handler: ${data}, length ${data.length}`,
+              `Client Socket message received : ${data}, length ${data.length}`,
             );
-            return yield* messages.offer(data);
+            return yield* PubSub.publish(messages, data);
           });
         })
         .pipe(
-          Effect.tap(() => Effect.log("WebSocket message received and queued")),
+          Effect.tap(() =>
+            Effect.log("Client Socket message received and queued"),
+          ),
         ),
     );
-    yield* Effect.log("WebSocket handler fiber started");
 
     /**
      * Send data through the WebSocket
-     * @param data Data to send (string, ArrayBuffer, or ArrayBufferView)
      */
     const sendMessage = (chunk: Uint8Array | string | Socket.CloseEvent) =>
       socket.writer.pipe(
         Effect.flatMap((write) => write(chunk)),
-        Effect.catchAll((error) =>
-          Effect.logError(`Failed to send message: ${error}`).pipe(
-            Effect.flatMap(() => Effect.fail(error)),
-          ),
-        ),
+        Effect.tapError((e) => Effect.logError(`Failed to send message: ${e}`)),
       );
 
     return {
@@ -55,6 +52,6 @@ export const createWebSocketConnection = (url: string) =>
       socket,
 
       // Access to the fiber running the socket
-      fiber,
+      publishMessageFiber,
     };
   });
