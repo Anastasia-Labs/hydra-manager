3cc18351152c1047f887cdf05feb78d6c9d5a095
Georgii Gerasev <george30032002@gmail.com>
2025-05-08 16:21:25 +0400

    WIP: init command

--- packages/cli/src/Command.ts
+++ packages/cli/src/Command.ts
@@ -4,33 +4,8 @@ import { Command, Options } from "@effect/cli"
 import { Effect, Option, Schedule, pipe } from "effect"
 
 export const initCommand = Command.make("init", {}).pipe(
-  Command.withHandler(runAction)
+  Command.withHandler(() => initHeadCommand)
 )
-
-function runAction() : Effect.Effect<void, Error, HydraHeadService> {
-  return Effect.gen(function* () {
-    const hydraHeadService = yield* HydraHeadService
-    const hydraHead = hydraHeadService.get
-
-    const policy = Schedule.fixed("1000 millis")
-    yield* Effect.retry(checkStatus(hydraHead), policy)
-    yield* Effect.tryPromise({ try : () => initHeadAction.value(hydraHead),
-                               catch: (e) => new Error(`Failed to init head with error: ${e}`)
-    })
-    hydraHead.mainNode.disconnect()
-  })}
-
-const checkStatus = (
-  hydraHead: HydraHead,
-): Effect.Effect<void, Error, never> => Effect.gen(function* () {
-        if (hydraHead.status == "IDLE") {
-          console.log(`Success with the status ${hydraHead.status}`)
-          return yield* Effect.void
-        } else {
-          console.log(`Failed with the status ${hydraHead.status}`)
-          return yield* Effect.fail(new Error(`Impossible to run Init with state: ${hydraHead.status}`))
-        }
-})
 
 export const initHeadCommand = Effect.gen(function* () {
     const hydraHead = yield* HydraHead
