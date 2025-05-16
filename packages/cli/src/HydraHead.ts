2f394472960d76ab11947cd5bd4628c68c46a067
Georgii Gerasev <george30032002@gmail.com>
2025-05-16 17:41:29 +0400

    WIP logBalances

--- packages/cli/src/HydraHead.ts
+++ packages/cli/src/HydraHead.ts
@@ -26,18 +26,7 @@ export class HydraHead extends Effect.Service<HydraHead>()("HydraHead", {
       providerLucidRetryPolicy,
     );
 
-    const getNodeFundsUTxOs = (nodeName: string) : Effect.Effect<Array<UTxO>, Error> => {
-      return Effect.gen(function* () {
-        const nodeConfig = yield* config.getNodeConfigByName(nodeName)
-        const address = yield* NodeConfig.skToAddress(nodeConfig.fundsWalletSK)
-        return yield* Effect.tryPromise({
-          try: () => providerLucidL1.utxosAt(address),
-          catch: (e) =>  new Error(`Failed to get UTxOs at ${address}: ${e}`),
-        })
-      })
-    }
-
-    const nodeNames = config.projectConfig.nodes.map((node) => node.name);
+    const nodeNames : Array<string> = config.projectConfig.nodes.map((node) => node.name);
     const nodeConfigs = yield* Effect.forEach(nodeNames, (name) =>
       config.getNodeConfigByName(name),
     );
@@ -82,6 +71,29 @@ export class HydraHead extends Effect.Service<HydraHead>()("HydraHead", {
           new Error(`Failed to find config for node with a name ${nodeName}`),
         );
       });
+
+      const getNodeFundsUTxOs = (nodeName: string) : Effect.Effect<Array<UTxO>, Error> => {
+        return Effect.gen(function* () {
+          const nodeConfig = yield* config.getNodeConfigByName(nodeName)
+          const address = yield* NodeConfig.skToAddress(nodeConfig.fundsWalletSK)
+          return yield* Effect.tryPromise({
+            try: () => providerLucidL1.utxosAt(address),
+            catch: (e) =>  new Error(`Failed to get UTxOs at ${address}: ${e}`),
+          })
+        })
+      }
+
+      const logBalances = () : Effect.Effect<void, Error> => {
+        return Effect.gen(function* () {
+          nodeNames.forEach((name) => {
+            Effect.gen(function* () {
+              const utxos = yield* getNodeFundsUTxOs(name)
+
+            })
+          })
+        })
+
+      }
 
     return {
       providerLucidL1,
