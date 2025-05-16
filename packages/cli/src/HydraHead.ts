f6312a7bd40debacb9ee2af337a7f1638fe26ef8
Georgii Gerasev <george30032002@gmail.com>
2025-05-16 17:50:53 +0400

    WIP

--- packages/cli/src/HydraHead.ts
+++ packages/cli/src/HydraHead.ts
@@ -85,9 +85,26 @@ export class HydraHead extends Effect.Service<HydraHead>()("HydraHead", {
 
       const logBalances = () : Effect.Effect<void, Error> => {
         return Effect.gen(function* () {
-          nodeNames.forEach((name) => {
-            Effect.gen(function* () {
-              const utxos = yield* getNodeFundsUTxOs(name)
+          nodeNames.forEach((nodeName) => {
+            return Effect.gen(function* () {
+              const nodeConfig = yield* config.getNodeConfigByName(nodeName)
+              const fundsAddress = yield* NodeConfig.skToAddress(nodeConfig.fundsWalletSK)
+              const nodeAddress = yield* NodeConfig.skToAddress(nodeConfig.nodeWalletSK)
+
+              const fundsUTxOs =  yield* Effect.tryPromise({
+                try: () => providerLucidL1.utxosAt(fundsAddress),
+                catch: (e) =>  new Error(`Failed to get UTxOs at ${fundsAddress}: ${e}`),
+              })
+              const nodeUTxOs =  yield* Effect.tryPromise({
+                try: () => providerLucidL1.utxosAt(nodeAddress),
+                catch: (e) =>  new Error(`Failed to get UTxOs at ${nodeAddress}: ${e}`),
+              })
+
+              const fundsBalance = fundsUTxOs.reduce((acc, utxo) => acc + utxo.assets["lovelace"].valueOf(), 0n) / 1000000n
+              const nodeBalance = nodeUTxOs.reduce((acc, utxo) => acc + utxo.assets["lovelace"].valueOf(), 0n) / 1000000n
+
+              Effect.log(`${nodeName} balances:` )
+              Effect.log(`  - funds address ${} balances:` )
 
             })
           })
